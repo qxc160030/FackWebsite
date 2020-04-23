03defcc38a878951b91f86102984f5c6940eab98
RayLu11 <38595010+RayLu11@users.noreply.github.com>
2020-04-22 20:27:09 -0500

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,11 @@
+
+
+function acceptTerm()
+{
+        document.querySelector('.bg-modal').style.display = "none";
+}
+function declineTerm()
+{
+        alert("You need accept term to continue visit website!");
+}
+
