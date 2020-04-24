3adcacc5088cefbb1a60d9a3a07192c77c055e01
Qinglin Chen <33017555+qxc160030@users.noreply.github.com>
2020-04-23 23:45:30 -0500

    Embedded terms and conditions within the web

--- page1.js
+++ page1.js
@@ -1,11 +1,21 @@
 
 
-function acceptTerm()
+function randomlize()
 {
-        document.querySelector('.bg-modal').style.display = "none";
-}
-function declineTerm()
-{
-        alert("You need accept term to continue visit website!");
+        randomNumber = Math.floor(Math.random() * 3)+1;
+        console.log(randomNumber)
+        if(randomNumber == 1)
+        {
+                document.getElementById("textbox1").style.display = "flex";
+        }
+        else if(randomNumber == 2)
+        {
+                document.getElementById("textbox2").style.display = "flex";
+        }
+        else if(randomNumber == 3)
+        {
+                document.getElementById("textbox3").style.display = "flex";
+        }
+        
 }
 
